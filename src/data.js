1c147645b8b4a4b72d2ba4fd9849587056edcb1b
Shraddha <shraddhasingh059@gmail.com>
2023-01-14 05:36:34 -0400

    Added data.js

--- src/data.js
+++ src/data.js
@@ -1,41 +1,35 @@
 export const featuredPortfolio = [
-    {
-      id: 1,
-      title: "Social Media App",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    },
-    {
-      id: 2,
-      title: "Rampa UI Design",
-      img:
-        "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
-    },
-    {
-      id: 3,
-      title: "E-commerce Web Design",
-      img:
-        "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
-    },
-    {
-      id: 4,
-      title: "Relax Mobile App",
-      img:
-        "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
-    },
-    {
-      id: 5,
-      title: "Hero Web Design",
-      img:
-        "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
-    },
-    {
-      id: 6,
-      title: "Banking App",
-      img:
-        "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
-    },
-  ];
+  {
+    id: 1,
+    title: "Chat Room App",
+    img: "https://github.com/shrad059/chat-room/blob/main/screenshot/Screenshot%202023-01-13%20at%209.12.38%20PM.png?raw=true",
+  },
+  {
+    id: 2,
+    title: "A* Path Finding Algorithm",
+    img: "https://github.com/shrad059/A-Path-Finding-Algorithm/raw/main/demo.gif",
+  },
+  {
+    id: 3,
+    title: "E-commerce MERN stack",
+    img: "https://github.com/shrad059/portfolio/blob/main/src/images/eCommerce-website.jpeg?raw=true",
+  },
+  {
+    id: 4,
+    title: "Tic-Tac-Toe",
+    img: "https://github.com/shrad059/portfolio/blob/main/src/images/tic-tac-toe.png?raw=true",
+  },
+  {
+    id: 5,
+    title: "Brickers Game",
+    img: "https://github.com/shrad059/portfolio/blob/main/src/images/brickers.png?raw=true",
+  },
+  {
+    id: 6,
+    title: "ML Text",
+    img: "https://github.com/shrad059/portfolio/blob/main/src/images/ML-text.png?raw=true",
+  },
+];
   
   export const webPortfolio = [
     {
